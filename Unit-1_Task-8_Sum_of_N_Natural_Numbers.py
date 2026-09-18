1c77cd9a497101fa2c9643a134687d90f0c09a1e
simhadrijashuva <simhadrijashuva1@gmail.com>
2026-09-18 14:12:58 +0530

    Add files via upload

---
+++ Unit-1_Task-8_Sum_of_N_Natural_Numbers.py
@@ -0,0 +1,4 @@
+#Write a python program to find the sum of first N natural numbers using a formula
+n=int(input("Enter a Number: "))
+sum_of_num=n * (n+1) / 2
+print(f"Sum of first {n} Natural numbers is {sum_of_num}")
