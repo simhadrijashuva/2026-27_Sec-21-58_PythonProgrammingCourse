5e6035b1f9b5d19aacfb1dfc19a8dd613a760ca9
simhadrijashuva <simhadrijashuva1@gmail.com>
2026-09-21 08:02:39 +0530

    Add files via upload

---
+++ if else condition.py
@@ -0,0 +1,6 @@
+# If and else condition
+marks=int(input("Enter marks of students : "))
+if marks>=35:
+    print(f"Result : Pass (marks = {marks})")
+else:
+    print(f"Result : Fail (marks = {marks})")
